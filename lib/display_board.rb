800ac93cb2315bef974f87ceeb2005215d6bfd70
Brandon Green <brandon@brandonleeproductions.com>
2018-05-03 02:48:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,7 @@
 def display_board
   puts "A Tic Tac Toe Board"
   
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 
 end
 display_board
